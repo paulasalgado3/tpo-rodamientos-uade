7fc16755d29ee6d211f25ab858fd1fff9c610411
paulasalgado3 <paulasalgado3@gmail.com>
2011-07-05 02:12:16 +0000

    clases para el jsp de generar sol de compra

--- Rodamientos/src/service/SolicitudCompraService.java
+++ Rodamientos/src/service/SolicitudCompraService.java
@@ -10,6 +10,7 @@ import modelo.SolicitudCompra;
 public interface SolicitudCompraService {
 
 	void generarSolicitudCompra(Set<Item> items, Cliente cli);
+	public Set<SolicitudCompra> generarSolicitudCompraSet(Set<Item> items, Cliente cli);
 	List<SolicitudCompra> findAll();
 	void confirmarSolicitudCompra(Integer id);
 	List<SolicitudCompra> obtenerNoConfirmadas();
--- Rodamientos/src/service/impl/SolicitudCompraServiceImpl.java
+++ Rodamientos/src/service/impl/SolicitudCompraServiceImpl.java
@@ -110,10 +110,97 @@ public class SolicitudCompraServiceImpl implements SolicitudCompraService{
 			cotDAO.update(c);
 		}
 		
-		
+	}
 	
+	public Set<SolicitudCompra> generarSolicitudCompraSet(Set<Item> items, Cliente cli){
+		Set<Item> itemsNoCotizados = new HashSet<Item>();
+		/*genero un set de solicitudes de compra ya que si hay items cotizados y 
+		 * sin cotizar ya son dos solicitudes, y si hay items de mas de una cotizacion es una solicitud por cada cotizacion
+		 * */
+		Set<SolicitudCompra> solicitudes = new HashSet<SolicitudCompra>();
+		
+		/*primero me fijo si el cliente tiene una o varias cotizaciones*/
+		List<Cotizacion> cotizaciones =cDAO.obtenerCotizacionCliente(cli);
+		
+		/*me fijo si los items corresponden a alguna de las cotizaciones anteriores*/
+		Set<ItemCotizacion> itemsCotizados = new HashSet<ItemCotizacion>();
+		for (Cotizacion c :cotizaciones){
+		
+			Set<Item> it = c.getItems();
+			for(Item i:it){
+				
+				for(Item ii:items){
+				
+					if((int)i.getId()==(int)ii.getId()){
+						/*si tiene el mismo id quiere decir que se cotizo previamente*/
+						
+						itemsCotizados.add(new ItemCotizacion(ii,c));
+						/*lo remuevo de la lista de items para que me queden unicamente los que no fueron cotizados*/
+						itemsNoCotizados.remove(ii);
+					}else{
+						
+					}
+				}
+			}
+			
+		}
+		
+		/*me quedan dos listas itemsCotizados(con el item y si cotizacion correspondiente) e items no cotizados*/
+		/*con los items no cotizados genero una cotizacion*/
+		if(!items.isEmpty()){
+			//CotizacionServiceImpl csi = new CotizacionServiceImpl();
+			//csi.generarCotizacion(cli.getDni(),items);
+			//COMENTADO PORQUE EL METODO DE GENERAR COTIZACION NO ESTA
 		
 		
+		}
+		/*CREAR SOLICITUD COMPRA (UNA SOLA POR COTIZACION)
+		 * PONERLE ITEMS
+		 * ACTUALIZAR COTIZACION CON EL NUMERO DE SOLICITUD DE COMPRA
+		 */
+		/*con los items cotizados genero las solicitudes de compra correspondientes (una por cotizacion) y actualizo las costizaciones con el numero de sol de compra correspondiente*/
+		Set<Cotizacion> cotizacionesActualizar = new HashSet<Cotizacion>();
+		Integer solicitudcotizaciongenerada =0;
+		for(ItemCotizacion ic:itemsCotizados){
+			solicitudcotizaciongenerada=0;
+			for(SolicitudCompra sol:solicitudes){
+				/*primer me fijo que no haya una solicitud ya creada para esa cotizacion*/
+				if((int)sol.getId_cotizacion()==(int)ic.getCot().getId()){
+					solicitudcotizaciongenerada=1;
+					sol.getItems().add(ic.getItem());
+					
+				}
+			}
+			//si no hay una solicitud para ese numero de cotizacion la creo
+			if(solicitudcotizaciongenerada==0){
+				
+				SolicitudCompra solicitud1 = new SolicitudCompra();
+				solicitud1.setCli(cli);
+				solicitud1.setItems(new HashSet<Item>());
+				solicitud1.getItems().add(ic.getItem());
+				solicitud1.setId_cotizacion(ic.getCot().getId());
+				solicitudes.add(solicitud1);
+				/*les seteo a las cotizaciones las solicitudes*/
+				Cotizacion c=cDAO.findById(ic.getCot().getId());
+			
+				c.setSolicitudCompra(solicitud1);
+				cotizacionesActualizar.add(c);
+				
+			}
+			
+			
+		}
+		CotizacionDAO cotDAO = new CotizacionDAOImpl();
+		/*guardo las solicitudes de compra*/
+		
+		for(SolicitudCompra s:solicitudes){
+			
+			sDAO.save(s);
+		}
+		for(Cotizacion c:cotizacionesActualizar){
+			cotDAO.update(c);
+		}
+		return solicitudes;
 		
 	}
 	public void confirmarSolicitudCompra(Integer idSolicitudCompra){
